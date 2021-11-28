1fab5845e8ded122de94dd36dbed63154f4175fa
hoodboy <littlepark0107@gmail.com>
2021-11-29 08:55:33 +0900

    fix: hoodboy profile

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,81 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Title, Subtitle} from '../components/ContentsAtoms';
-import ProfileImage from '../images/ZEPETO_CAPTURE.png';
-import HoonImage from '../images/hoon.jpg';
-import PageWrapper from '../components/PageWrapper';
-import PlusImage from '../images/plus.png';
-import CodeAnimation from '../components/CodeAnimation';
+import React from "react";
+import styled from "styled-components";
+import { Title, Subtitle } from "../components/ContentsAtoms";
+import ProfileImage from "../images/ZEPETO_CAPTURE.png";
+import PageWrapper from "../components/PageWrapper";
+import PlusImage from "../images/plus.png";
+import CodeAnimation from "../components/CodeAnimation";
 
 const Profile = () => {
-    return (
-        <PageWrapper>
-            <ProfileBlock>
-                <ProfileImg/>
-                <PlusImg/>
-                <HoonImg/>
-            </ProfileBlock>
-            <IntroduceBlock>
-                <Title>김태철</Title>
-                <Subtitle>4.16교육혁명은 자기 통찰로부터 출발하여 심리적 욕망을 바꾸는 사상운동이며 학생,학부모,선생님이 모두 웃는 하하하교육혁명으로 스며들어야 한다.</Subtitle>
-            </IntroduceBlock>
-        </PageWrapper>
-    );
-}
+  return (
+    <PageWrapper>
+      <ProfileBlock>
+        <ProfileImg />
+        <PlusImg />
+        <CodeAnimation />
+      </ProfileBlock>{" "}
+      <IntroduceBlock>
+        <Title> HoodBoy </Title>{" "}
+        <Subtitle>
+          {" "}
+          Enjoy the web and try to implement everything I imagine in code.{" "}
+        </Subtitle>{" "}
+      </IntroduceBlock>{" "}
+    </PageWrapper>
+  );
+};
 
 export default Profile;
 
 const ProfileBlock = styled.div`
-    width: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    margin: 0 auto;
-    padding: 3vw 0;
-    @media (max-width: 560px) {
-        width: 90%;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 0 auto;
+  padding: 3vw 0;
+  @media (max-width: 560px) {
+    width: 90%;
   }
 `;
 
 const ProfileImg = styled.div`
-    width: 200px;
-    height: 200px;
-    min-width: 150px;
-    min-height: 150px;
-    background-image: url(${ProfileImage});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 200px;
+  height: 200px;
+  min-width: 150px;
+  min-height: 150px;
+  background-image: url(${ProfileImage});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const HoonImg = styled(ProfileImg)`
-    height: 150px;
-    margin-top: auto;
-    background-image: url(${HoonImage});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const PlusImg = styled(ProfileImg)`    
-    width: 50px;
-    height: 50px;
-    min-width: 30px;
-    min-height: 30px;
-    background-image: url(${PlusImage});
+const PlusImg = styled(ProfileImg)`
+  width: 50px;
+  height: 50px;
+  min-width: 30px;
+  min-height: 30px;
+  background-image: url(${PlusImage});
 `;
 
 const IntroduceBlock = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 0 3vw;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 3vw;
+  text-align: center;
 `;
--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,139 +1,147 @@
-import React from 'react';
-import styled from 'styled-components';
-import PageWrapper from '../components/PageWrapper';
-import {Title, Subtitle, TitleBlock, Content, InfoBlock} from '../components/ContentsAtoms';
-import ReactImage from '../images/reactlogo.png'
-import TsImage from '../images/tslogo.png';
-import JsImage from '../images/jslogo.png';
-import ReduxImage from '../images/reduxlogo.png';
-import StyledImage from '../images/styledlogo.png';
-import AntdImage from '../images/antdlogo.png';
-import MemoryImage from '../images/memorylogo.png';
-import DusImage from '../images/duslogo.png';
+import React from "react";
+import styled from "styled-components";
+import PageWrapper from "../components/PageWrapper";
+import {
+  Title,
+  Subtitle,
+  TitleBlock,
+  Content,
+  InfoBlock,
+} from "../components/ContentsAtoms";
+import ReactImage from "../images/reactlogo.png";
+import TsImage from "../images/tslogo.png";
+import JsImage from "../images/jslogo.png";
+import ReduxImage from "../images/reduxlogo.png";
+import StyledImage from "../images/styledlogo.png";
+import AntdImage from "../images/antdlogo.png";
 
 const Profile = () => {
-    return (
-        <PageWrapper>
-        <TitleBlock>
-            <Title>Skills</Title>
-            <Subtitle>These are skills that I have to learn to shape my own thoughts. Not all technologies can be used professionally, but they can be used without much difficulty in proceeding with the project.</Subtitle>
-            <InfoBlock>
-                <TechWrapper>
-                    <AntdLogo/>
-                    <Wrapper>
-                        <TechTitle>동북고등학교</TechTitle>
-                        <Content>동북고등학교 1985년 졸업</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <ReactLogo/>
-                    <Wrapper>
-                        <TechTitle>연세대학교</TechTitle>
-                        <Content>연세대학교에서 북한학 전공</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <TsLogo/>
-                    <Wrapper>
-                        <TechTitle>가천대학교</TechTitle>
-                        <Content>가천대학교에서 국어국문학전공 전공, 문학박사 현대시, 현대문학도 전공함 1994년~1999년</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <DusLogo/>
-                    <Wrapper>
-                        <TechTitle>연대한국학교</TechTitle>
-                        <Content>연대한국학교에서 2014년 ~ 2016년 근무</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <JsLogo/>
-                    <Wrapper>
-                        <TechTitle>한국디지털미디어고등학교</TechTitle>
-                        <Content>한국디지털미디어고등학교 국어교사</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <ReduxLogo/>
-                    <Wrapper>
-                        <TechTitle>4.16 교육연구소</TechTitle>
-                        <Content>4.16 교육연구소 연구소 소장</Content>
-                    </Wrapper>
-                </TechWrapper>
-                <TechWrapper>
-                    <MemoryLogo/>
-                    <Wrapper>
-                        <TechTitle>4.16 기억저장소</TechTitle>
-                        <Content>4.16 기억저장소 운영위원</Content>
-                    </Wrapper>
-                </TechWrapper>
-            </InfoBlock>
-        </TitleBlock>
-        </PageWrapper>
-    );
-}
+  return (
+    <PageWrapper>
+      <TitleBlock>
+        <Title>Skills</Title>
+        <Subtitle>
+          These are skills that I have to learn to shape my own thoughts. Not
+          all technologies can be used professionally, but they can be used
+          without much difficulty in proceeding with the project.
+        </Subtitle>
+        <InfoBlock>
+          <TechWrapper>
+            <ReactLogo />
+            <Wrapper>
+              <TechTitle>React</TechTitle>
+              <Content>
+                React is developed on a component basis to facilitate
+                productivity and maintenance.
+              </Content>
+            </Wrapper>
+          </TechWrapper>
+          <TechWrapper>
+            <TsLogo />
+            <Wrapper>
+              <TechTitle>TypeScript</TechTitle>
+              <Content>
+                TypeScript supports static types, allowing errors to be detected
+                in the compilation phase.
+              </Content>
+            </Wrapper>
+          </TechWrapper>
+          <TechWrapper>
+            <JsLogo />
+            <Wrapper>
+              <TechTitle>JavaScript</TechTitle>
+              <Content>
+                It is an essential language for creating a dynamic web and is
+                essential for React and Typescripts.
+              </Content>
+            </Wrapper>
+          </TechWrapper>
+          <TechWrapper>
+            <ReduxLogo />
+            <Wrapper>
+              <TechTitle>Redux</TechTitle>
+              <Content>Use to efficiently manage state in a project.</Content>
+            </Wrapper>
+          </TechWrapper>
+          <TechWrapper>
+            <StyledLogo />
+            <Wrapper>
+              <TechTitle>StyledComponent</TechTitle>
+              <Content>
+                It can be changed by lowering the props fluidly, inheriting
+                styles, declaring variables, etc.
+              </Content>
+            </Wrapper>
+          </TechWrapper>
+          <TechWrapper>
+            <AntdLogo />
+            <Wrapper>
+              <TechTitle>AntDesign</TechTitle>
+              <Content>
+                Ant Design is a clean, sophisticated design that meets both web
+                and mobile at the same time.
+              </Content>
+            </Wrapper>
+          </TechWrapper>
+        </InfoBlock>
+      </TitleBlock>
+    </PageWrapper>
+  );
+};
 
 export default Profile;
 
 const TechWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    width: 60vw;
-    padding: 5vh 0;
-    @media (max-width: 560px) {
-        width: 100%;
-  } 
+  display: flex;
+  align-items: center;
+  width: 60vw;
+  padding: 5vh 0;
+  @media (max-width: 560px) {
+    width: 100%;
+  }
 `;
 
 const Wrapper = styled.div`
-    padding: 0 3vw;
-    height: 100%;
+  padding: 0 3vw;
+  height: 100%;
 `;
 
 const TsLogo = styled.div`
-    width: 5vw;
-    height: 5vw;
-    min-width: 80px;
-    min-height: 80px;
-    background-image: url(${TsImage});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  width: 5vw;
+  height: 5vw;
+  min-width: 80px;
+  min-height: 80px;
+  background-image: url(${TsImage});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ReactLogo = styled(TsLogo)`
-    background-image: url(${ReactImage});
+  background-image: url(${ReactImage});
 `;
 
 const JsLogo = styled(TsLogo)`
-    background-image: url(${JsImage});
+  background-image: url(${JsImage});
 `;
 
 const StyledLogo = styled(TsLogo)`
-    background-image: url(${StyledImage});
+  background-image: url(${StyledImage});
 `;
 
 const AntdLogo = styled(TsLogo)`
-    background-image: url(${AntdImage});
+  background-image: url(${AntdImage});
 `;
 
 const ReduxLogo = styled(TsLogo)`
-    background-image: url(${ReduxImage});
-`;
-
-const MemoryLogo = styled(TsLogo)`
-    background-image: url(${MemoryImage});
-`;
-
-const DusLogo = styled(TsLogo)`
-    background-image: url(${DusImage});
+  background-image: url(${ReduxImage});
 `;
 
 const TechTitle = styled.div`
-    font-size: 3vh;
-    font-weight: 700;
-    color: #fff;
+  font-size: 3vh;
+  font-weight: 700;
+  color: #fff;
 `;
